55c7360aad35fff3a17a7fc5e00a3dae57d098d6
amihaylovaa <aggmihaylova@gmail.com>
2021-05-02 19:51:35 +0300

    change port, add timeout, use geo coordinates

--- camera_app.py
+++ camera_app.py
@@ -1,38 +1,37 @@
+from flask import Flask
+from flask import send_file
+from flask import Response
+from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
+import base64
 import re
 import datetime
 import serial 
-from flask import Flask
-from flask import send_file
-from flask_restful import Resource, Api
-from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
 def get_latitude(lat, lat_direction):
-    lat_degree = (float)(lat[0:2])
-    lat_min = ((float)(lat[2:]))/60.0
-    latitude=0
-    if(lat_direction=='S'):
-     return -(lat_degree+lat_min)
-    else:
-     return lat_degree+lat_min
+    lat_degree = lat[0:2]
+    lat_min = lat[2:]
 
+    return lat_degree+'° '+lat_min+ '\' '+ lat_direction
+    
 def get_longitude(lon, lon_direction):
     lon_degree = 0
     lon_min = 0
 
     if(len(lon)==7):
-     lon_degree = (float)(lon[0:2])
-     lon_min = ((float)(lon[2:]))/60
+     lon_degree = lon[0:2]
+     lon_min = lon[2:]
     else:
-     lon_degree =(float) (lon[0:3])
-     lon_min = ((float)(lon[3:]))/60
-    if(lon_direction=='W'):
-     return -(lon_degree+lon_min)
-    else:
-     return lon_degree+lon_min
+     lon_degree = lon[0:3]
+     lon_min = lon[3:]
+    
+     return lon_degree+'° '+lon_min+ '\' '+ lon_direction
+    
     
 def get_date_time(time_utc, date_gps):
     date = date_gps[0:2] + "-" + date_gps[2:4] + "-"+date_gps[4:]+""
@@ -41,8 +40,8 @@ def get_date_time(time_utc, date_gps):
     return date+" "+time
       
 def read_GPS_data():
-  port = serial.Serial('/dev/ttyS0', 9600)
-  line = port.readline(1000) 
+  port = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+  line = port.readline()
   start_tag = 0
   end_tag =  6  
   if ("$GPRMC"==line[start_tag:end_tag]): 
@@ -60,7 +59,6 @@ def read_GPS_data():
 
     return "latitude:"+str(latitude)+", longitude:"+str(longitude)+", "+date_time+""
 
-# TODO - form response
 class Picture(Resource):
     def get(self):
         pass
